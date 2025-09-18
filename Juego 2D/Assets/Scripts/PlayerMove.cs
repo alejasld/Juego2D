d0e235e4a227737adf8f6073562da096bcd62fc2
alejasld <alejandra.sepulveda@uao.edu.co>
2025-09-18 18:09:41 -0500

    create animation and finish
    Se creo la animacion del personaje y se hizo el cambio de escena al tocar la bandera

--- Juego 2D/Assets/Scripts/PlayerMove.cs
+++ Juego 2D/Assets/Scripts/PlayerMove.cs
@@ -1,26 +1,82 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using static UnityEditor.Searcher.SearcherWindow.Alignment;
+
 
 public class PlayerMove : MonoBehaviour
 {
-
     private Rigidbody2D _Rigidbody2D;
-    float horizontal;   
-    public float speed=5f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Animator animator;
+
+    float horizontal;
+    public float speed = 5f;
+    public float jumpForce = 3f;
+    private bool isGrounded;
+
+    [Header("Escena a cargar cuando toca la bandera")]
+    public string nextSceneName; // ← Nombre de la siguiente escena
+
     void Start()
     {
-        _Rigidbody2D=GetComponent<Rigidbody2D>();
+        _Rigidbody2D = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        horizontal=Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        float currentSpeed = Mathf.Abs(_Rigidbody2D.linearVelocity.x);
+        animator.SetFloat("Speed", currentSpeed);
+
+        // Saltar
+        if (Input.GetKeyDown(KeyCode.UpArrow) && isGrounded)
+        {
+            _Rigidbody2D.linearVelocity = new Vector2(_Rigidbody2D.linearVelocity.x, jumpForce);
+            animator.SetBool("isJumping", true); // Activar animación de salto
+        }
+
+        // Si está en el suelo, desactivar animación de salto
+        if (isGrounded)
+        {
+            animator.SetBool("isJumping", false);
+        }
     }
 
-    private void FixedUpdate() 
+    void FixedUpdate()
     {
-        _Rigidbody2D.linearVelocity =new Vector2(horizontal*speed, _Rigidbody2D.linearVelocity.y);
-    }   
+        _Rigidbody2D.linearVelocity = new Vector2(horizontal * speed, _Rigidbody2D.linearVelocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Finish"))
+        {
+            // Cargar la escena indicada en el Inspector
+            if (!string.IsNullOrEmpty(nextSceneName))
+            {
+                SceneManager.LoadScene(nextSceneName);
+            }
+            else
+            {
+                Debug.LogWarning("No se asignó ninguna escena en 'nextSceneName'");
+            }
+        }
+    }
 }
